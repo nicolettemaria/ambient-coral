040679fe6278c9d8c54b0f2a5092332e59cf582e
nicolettemaria <nicolettetovstashy@gmail.com>
2021-03-10 22:16:16 -0500

    Added hue api request bones

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,18 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    // processing
     implementation("org.processing:core:3.3.7")
+
+    // processing (serial lib)
     implementation("org.processing:serial:3.3.7")
     implementation("io.github.java-native:jssc:2.9.2")
+
+    // network
     implementation("io.ktor:ktor-client:1.5.1")
     implementation("io.ktor:ktor-client-cio:1.5.1")
+    implementation("io.ktor:ktor-client-jackson:1.5.1")
 }
 
 tasks {
--- src/main/kotlin/com/nicolettetovstashy/coral/Main.kt
+++ src/main/kotlin/com/nicolettetovstashy/coral/Main.kt
@@ -1,19 +1,38 @@
 package com.nicolettetovstashy.coral
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.nicolettetovstashy.coral.sketch.Sketch
 import io.ktor.client.*
+import io.ktor.client.features.json.*
 import io.ktor.client.request.*
+import io.ktor.http.*
 import kotlinx.coroutines.runBlocking
 
 const val bridgeIp = "192.168.1.4"
 const val username = "AIgdIkyTOmYXbkxLKJSRiVWz8Nl15flPoiyNDDO0"
 
 fun main(args: Array<String>) {
-    val client = HttpClient()
+    val client = HttpClient() {
+        install(JsonFeature) {
+            serializer = JacksonSerializer()
+        }
+    }
 
     runBlocking {
-        val lights = client.get<String>("http://$bridgeIp/api/$username/lights")
-        println(lights)
+        val lights = client.get<JsonNode>("http://$bridgeIp/api/$username/lights")
+        println(lights.toPrettyString())
+
+        val currentLight = 4
+
+        client.put<String>("http://$bridgeIp/api/$username/lights/$currentLight/state") {
+            contentType(ContentType.Application.Json)
+            body = mapOf(
+                "on" to true,
+                "sat" to 254,
+                "bri" to 254,
+                "xy" to listOf(0.675,0.8)
+            )
+        }
     }
 
     // Sketch.run()
